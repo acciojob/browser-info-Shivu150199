0677b6c99a1e55f1e4533237a7fc182548a9ef0d
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2023-09-21 19:54:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let appName=window.navigator.appName;
+let version=window.navigatot.appVersion
+
+browser_info.innerText=`You are using ${appName} version ${version}`;
